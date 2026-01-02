7af8cd0cdb974e24fc7068b7d8fc03a75b6d2a17
Meeta Panjwani <meeta@example.com>
2026-01-02 18:29:11 +0530

    frontend initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const API_URL = "https://expense-tracker-backend.onrender.com";
+const API_EXPENSE = `${API_URL}/api/expenses`;
+
+
+// REGISTER
+async function registerUser() {
+  const name = document.getElementById("r-name").value;
+  const email = document.getElementById("r-email").value;
+  const password = document.getElementById("r-password").value;
+
+  try {
+    const res = await fetch(`${API_AUTH}/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, email, password })
+    });
+
+    const data = await res.json();
+    alert("Registered Successfully ✅");
+
+  } catch (err) {
+    alert("Register failed ❌");
+    console.error(err);
+  }
+}
+
+// LOGIN
+async function loginUser() {
+  const email = document.getElementById("l-email").value;
+  const password = document.getElementById("l-password").value;
+
+  try {
+    const res = await fetch(`${API_AUTH}/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password })
+    });
+
+    const data = await res.json();
+
+    if (data.token) {
+      localStorage.setItem("token", data.token);
+      window.location.href = "dashboard.html"; // ✅ REDIRECT
+    } else {
+      alert("Invalid login ❌");
+    }
+
+  } catch (err) {
+    alert("Login failed ❌");
+    console.error(err);
+  }
+}
+
+
